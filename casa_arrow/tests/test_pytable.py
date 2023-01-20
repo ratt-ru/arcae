0eb8a6689efe73cbe332e98e3eaaefa5fdb99668
Simon Perkins <simon.perkins@gmail.com>
2023-01-20 13:30:36 +0200

    Make table read a fixture

--- casa_arrow/tests/test_pytable.py
+++ casa_arrow/tests/test_pytable.py
@@ -2,14 +2,16 @@ import pytest
 
 from casa_arrow import pytable
 
+@pytest.fixture(scope="module")
+def arrow_table():
+    return pytable.open_table("/home/simon/data/WSRT_polar.MS_p0/")
+
 @pytest.mark.xfail(reason="https://github.com/apache/arrow/issues/32291 and https://github.com/apache/arrow/pull/10565#issuecomment-885786527")
-def test_complex_type_access_fail():
-    table = pytable.open_table("/home/simon/data/WSRT_polar.MS_p0/")
-    data = table.column("DATA")
+def test_complex_type_access_fail(arrow_table):
+    data = arrow_table.column("DATA")
     print(data[0])
 
-def test_pytable():
-    table = pytable.open_table("/home/simon/data/WSRT_polar.MS_p0/")
-    uvw = table.column("UVW")
+def test_pytable(arrow_table):
+    uvw = arrow_table.column("UVW")
     print(uvw[16])
 
