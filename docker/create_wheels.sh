a625ace76b8f2eab8193ae7ac9d8b49f017a383f
Simon Perkins <simon.perkins@gmail.com>
2023-03-03 13:46:28 +0200

    Docker wheel creation

---
+++ docker/create_wheels.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ve
+
+HERE=`dirname "$0"`
+cd $HERE/..
+
+for i in 39 310; do
+    docker build -f docker/py${i}-wheel-manylinux2014.docker . -t casa-arrow-py${i}-wheel
+    dockerid=$(docker create casa-arrow-py${i}-wheel)
+    docker cp ${dockerid}:/wheels/ wheel-${i}
+    docker rm ${dockerid}
+done
