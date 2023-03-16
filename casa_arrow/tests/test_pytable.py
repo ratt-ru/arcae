c3ffe2835e968e8484ae127baaac41c90c2051b4
Simon Perkins <simon.perkins@gmail.com>
2023-03-16 11:18:42 +0200

    Sort files

--- casa_arrow/tests/test_pytable.py
+++ casa_arrow/tests/test_pytable.py
@@ -67,7 +67,7 @@ def test_print_dataset_structure(partitioned_dataset):
         indent = " " * 4 * (level)
         print("{}{}/".format(indent, os.path.basename(root)))
         subindent = " " * 4 * (level + 1)
-        for f in files:
+        for f in sorted(files):
             print("{}{}".format(subindent, f))
 
 
