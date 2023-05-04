017e4f3a65b085b00619ea194ed614efbe323fdd
Simon Perkins <simon.perkins@gmail.com>
2023-05-04 13:28:31 +0200

    Upgrade to pyarrow 12.0.0

--- docker/create_wheels.sh
+++ docker/create_wheels.sh
@@ -3,7 +3,7 @@
 HERE=`dirname "$0"`
 cd $HERE/..
 
-for i in 38 39 310; do
+for i in 39; do
     docker build -f docker/py${i}-wheel-manylinux2014.docker . -t casa-arrow-py${i}-wheel
     dockerid=$(docker create casa-arrow-py${i}-wheel)
     docker cp ${dockerid}:/wheels/ wheel-${i}
