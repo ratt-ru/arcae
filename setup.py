4b24f822892680626def8645587768d6dc2e7d1e
Simon Perkins <simon.perkins@gmail.com>
2023-05-31 14:10:40 +0200

    Cython shared object builds and works

--- setup.py
+++ setup.py
@@ -1,64 +1,39 @@
 import os
+from os.path import join as pjoin
 from skbuild import setup
 
-# from Cython.Build import cythonize
 import numpy as np
 import pyarrow as pa
 
-
-def create_extensions():
-    casa_libs = ["casa_derivedmscal", "casa_meas", "casa_ms", "casa_tables"]
-
-    pa.create_library_symlinks()
-    include_dirs = [np.get_include(), pa.get_include()]
-    library_dirs = pa.get_library_dirs()
-    libraries = casa_libs + pa.get_libraries()
-
-    with open("build_log.txt", "w") as f:
-        print(f"include_dirs={include_dirs}", file=f)
-        print(f"library_dirs={library_dirs}", file=f)
-        print(f"libraries={libraries}", file=f)
-
-    ext_modules = [Extension("casa_arrow._arrow_tables", ["casa_arrow/*.pyx"])]
-    ext_modules = cythonize(ext_modules)
-
-    for ext in ext_modules:
-        # The Numpy C headers are currently required
-        ext.include_dirs.extend(include_dirs)
-        ext.libraries.extend(libraries)
-        ext.library_dirs.extend(library_dirs)
-
-        if os.name == "posix":
-            ext.extra_compile_args.extend(["--std=c++17"])
-
-    root = "casa_arrow"
-
-    return {
-            "ext_modules": ext_modules,
-            "packages": [root] + [f"{root}.{item}" for item in find_packages(where=root)],
-            "include_package_data": True,
-     }
-
 if VCPKG_ROOT := os.environ.get("VCPKG_ROOT"):
-    toolchain = os.path.join(VCPKG_ROOT, "scripts", "buildsystems", "vcpkg.cmake")
+    toolchain = pjoin(VCPKG_ROOT, "scripts", "buildsystems", "vcpkg.cmake")
 
     if not os.path.exists(toolchain):
         raise ValueError("Toolchain {toolchain} does not exist")
 else:
     raise ValueError("VCPKG_ROOT not set")
 
-
 pa.create_library_symlinks()
+CI_VCPKG_PATH = pjoin(os.getcwd(), "ci", "vcpkg")
+OVERLAY_TRIPLETS = pjoin(CI_VCPKG_PATH, "custom-triplets")
+PORT_DIR = pjoin(CI_VCPKG_PATH, "custom-ports")
+CUSTOM_PORTS = ["casacore"]
+CUSTOM_PORT_PATHS = [pjoin(PORT_DIR, port) for port in CUSTOM_PORTS]
+OVERLAY_PORTS = os.path.pathsep.join(CUSTOM_PORT_PATHS)
 
 CMAKE_ARGS = [
-    " -DCMAKE_VERBOSE_MAKEFILE=ON",
     f"-DNUMPY_INCLUDE={np.get_include()}",
     f"-DPYARROW_INCLUDE={pa.get_include()}",
     f"-DPYARROW_LIBDIRS={' '.join(pa.get_library_dirs())}",
     f"-DPYARROW_LIBS={' '.join(pa.get_libraries())}",
-    f"-DCMAKE_TOOLCHAIN_FILE={toolchain}"]
-
+    f"-DCMAKE_TOOLCHAIN_FILE={toolchain}",
+    f"-DVCPKG_OVERLAY_TRIPLETS={OVERLAY_TRIPLETS}",
+    f"-DVCPKG_OVERLAY_PORTS={OVERLAY_PORTS}",
+    f"-DVCPKG_TARGET_TRIPLET=x64-linux-dynamic-cxx17-abi0",
+    f"-DVCPKG_HOST_TRIPLET=x64-linux-dynamic-cxx17-abi0",
+    f"-S {CI_VCPKG_PATH}"]
 setup(
     packages=["casa_arrow"],
-    cmake_args=CMAKE_ARGS
+    cmake_args=CMAKE_ARGS,
+    cmake_source_dir=CI_VCPKG_PATH
 )
