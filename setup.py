76284cb5c129efa19349377fd06a0b0c197bc741
Simon Perkins <simon.perkins@gmail.com>
2023-06-20 20:01:31 +0200

    Handle https://github.com/scikit-build/scikit-build/issues/521

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
-from skbuild import setup
-from setuptools import find_packages
+from pathlib import Path
 import pyarrow as pa
+import re
+from setuptools import find_packages
+from skbuild import setup
 
 pa.create_library_symlinks()
+
+# https://github.com/scikit-build/scikit-build/issues/521
+for i in (Path(__file__).resolve().parent / "_skbuild").rglob("CMakeCache.txt"):
+    i.write_text(re.sub("^//.*$\n^[^#].*build-env.*$", "", i.read_text(), flags=re.M))
 
 setup(packages=find_packages())
