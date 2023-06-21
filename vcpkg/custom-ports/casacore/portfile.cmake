53343f20c5e0d370c6da87cd15565df07a813deb
Simon Perkins <simon.perkins@gmail.com>
2023-06-21 14:59:42 +0200

    Use newer vcpkg_cmake_config_fixup

--- vcpkg/custom-ports/casacore/portfile.cmake
+++ vcpkg/custom-ports/casacore/portfile.cmake
@@ -60,7 +60,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 vcpkg_fixup_pkgconfig()
-vcpkg_fixup_cmake()
+vcpkg_cmake_config_fixup()
 
 vcpkg_install_copyright(FILE_LIST "${src}/COPYING")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
