b64a7294913d3d4307d67cdf8233bb414b4c2ea6
Simon Perkins <simon.perkins@gmail.com>
2023-06-29 17:32:15 +0200

    config -> global_config

--- arcae/tests/test_pytable.py
+++ arcae/tests/test_pytable.py
@@ -197,45 +197,45 @@ def test_duckdb(partitioned_dataset):
 
 def test_config():
     from arcae.lib.arrow_tables import Configuration
-    config = Configuration()
+    global_config = global_configuration()
 
-    assert config["validate-level"] == "full"
+    assert global_config["validate-level"] == "full"
 
-    config["blah"] = "foo"
-    assert config["blah"] == "foo"
-    assert len(config) == 2
+    global_config["blah"] = "foo"
+    assert global_config["blah"] == "foo"
+    assert len(global_config) == 2
 
-    config["qux"] = "bar"
-    assert config["qux"] == "bar"
-    assert len(config) == 3
+    global_config["qux"] = "bar"
+    assert global_config["qux"] == "bar"
+    assert len(global_config) == 3
 
     try:
-        config["foo"] == "bar"
+        global_config["foo"] == "bar"
     except KeyError as e:
         assert "foo" in e.args[0]
 
-    assert config.get("foo") is None
-    assert config.get("foo", "bar") == "bar"
+    assert global_config.get("foo") is None
+    assert global_config.get("foo", "bar") == "bar"
 
-    assert list(config.items()) == [("blah", "foo"), ("qux", "bar"), ("validation-level", "full")]
+    assert list(global_config.items()) == [("blah", "foo"), ("qux", "bar"), ("validation-level", "full")]
 
     try:
-        config["foo"] = 1
+        global_config["foo"] = 1
     except TypeError as e:
         assert "(expected str, got int)" in e.args[0]
 
-    del config["blah"]
-    del config["foo"]
-    del config["qux"]
+    del global_config["blah"]
+    del global_config["foo"]
+    del global_config["qux"]
 
-    assert len(config) == 1
+    assert len(global_config) == 1
 
 
 def test_config_context_mgr():
     from arcae.lib.arrow_tables import Configuration
     from arcae import config
     global_config = Configuration()
-    assert len(global_config) == 1 and list(config.items()) == [("validation-level", "full")]
+    assert len(global_config) == 1 and list(global_config.items()) == [("validation-level", "full")]
 
     with config.set(*{"foo": "bar", "qux-baz": "blah"}):
         assert global_config["foo"] == "foo"
