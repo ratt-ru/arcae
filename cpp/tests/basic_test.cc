591a9c51d35b0b549b1975bb78c9393ad37fbab4
Simon Perkins <simon.perkins@gmail.com>
2023-10-12 11:19:05 +0200

    Add the most basic test case involving arcae and casacore interaction

--- cpp/tests/basic_test.cc
+++ cpp/tests/basic_test.cc
@@ -1,9 +1,20 @@
+
+#include <arcae/safe_table_proxy.h>
+#include <arrow/testing/gtest_util.h>
+#include <casacore/tables/Tables.h>
+#include <casacore/ms/MeasurementSets/MeasurementSet.h>
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+// Basic creation of a SafeTableProxy
+TEST(BasicTableTests, CasaTable) {
+
+  auto factory = []() -> arrow::Result<std::shared_ptr<casacore::TableProxy>> {
+    auto table_desc = casacore::TableDesc(casacore::MeasurementSet::requiredTableDesc());
+    auto setup_new_table = casacore::SetupNewTable("test.table", table_desc, casacore::Table::New);
+    auto ms = casacore::MeasurementSet(setup_new_table, 100);
+    return std::make_shared<casacore::TableProxy>(ms);
+  };
+
+  ASSERT_OK_AND_ASSIGN(auto table_proxy, arcae::SafeTableProxy::Make(factory));
+  ASSERT_EQ(table_proxy->nRow(), 100);
 }
