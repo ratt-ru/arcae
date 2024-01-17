3de6dcb8127bbf8725e946a29f6e132616ebe9dc
Simon Perkins <simon.perkins@gmail.com>
2024-01-17 23:09:26 +0200

    fix osx triplets

--- vcpkg/overlay-triplets/arm64-osx-dynamic-cxx17-abi1-dbg.cmake
+++ vcpkg/overlay-triplets/arm64-osx-dynamic-cxx17-abi1-dbg.cmake
@@ -1,6 +1,6 @@
 set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_BUILD_TYPE release)
 
 set(CMAKE_CXX_STANDARD 17)
@@ -12,3 +12,4 @@ set(VCPKG_CXX_FLAGS "${VCPKG_CXX_FLAGS} -D_GLIBCXX_USE_CXX11_ABI=1")
 # Add compiler specific debug flags here too
 
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
+set(VCPKG_OSX_ARCHITECTURES arm64)
