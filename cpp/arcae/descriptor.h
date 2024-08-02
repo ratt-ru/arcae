45b5c0a0c5529ac2d43aa67a14492d98364ccebb
Simon Perkins <simon.perkins@gmail.com>
2024-08-02 15:51:47 +0200

    clang-format descriptor.{h,cc}

--- cpp/arcae/descriptor.h
+++ cpp/arcae/descriptor.h
@@ -6,13 +6,12 @@
 
 namespace arcae {
 
-arrow::Result<std::string> MSDescriptor(
-    const std::string & table, bool complete=false);
+arrow::Result<std::string> MSDescriptor(const std::string &table,
+                                        bool complete = false);
 
-arrow::Result<casacore::SetupNewTable> DefaultMSFactory(
-    const std::string & name,
-    const std::string & subtable,
-    const std::string & json_table_desc="{}",
-    const std::string & json_dminfo="{}");
+arrow::Result<casacore::SetupNewTable>
+DefaultMSFactory(const std::string &name, const std::string &subtable,
+                 const std::string &json_table_desc = "{}",
+                 const std::string &json_dminfo = "{}");
 
 } // namespace arcae
