8d466061962cd987484799e5af6d5f4516079d07
Simon Perkins <simon.perkins@gmail.com>
2024-09-27 16:28:20 +0200

    Add the test case outline

---
+++ cpp/tests/group_sort_test.cc
@@ -0,0 +1,48 @@
+#include "arcae/group_sort.h"
+
+#include <memory>
+
+#include <arrow/api.h>
+#include <arrow/ipc/json_simple.h>
+#include <arrow/result.h>
+#include <arrow/testing/gtest_util.h>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+using ::arrow::ipc::internal::json::ArrayFromJSON;
+
+using ::arcae::GroupSortData;
+using ::arcae::MergeGroups;
+
+namespace {
+
+TEST(GroupSortTest, TestSort) {
+  std::vector<std::shared_ptr<arrow::Array>> groups;
+
+  ASSERT_OK_AND_ASSIGN(auto group1,
+                       ArrayFromJSON(arrow::int32(), "[1, 1, 1, 2, 2, 2, 3, 3, 3]"));
+  ASSERT_OK_AND_ASSIGN(auto group2,
+                       ArrayFromJSON(arrow::int32(), "[1, 1, 1, 2, 2, 2, 3, 3, 3]"));
+  ASSERT_OK_AND_ASSIGN(auto group3,
+                       ArrayFromJSON(arrow::int32(), "[1, 1, 1, 2, 2, 2, 3, 3, 3]"));
+
+  groups.push_back(group1);
+  groups.push_back(group2);
+  groups.push_back(group3);
+
+  ASSERT_OK_AND_ASSIGN(auto time,
+                       ArrayFromJSON(arrow::float64(), "[1, 2, 3, 4, 5, 6, 7, 8, 9]"));
+  ASSERT_OK_AND_ASSIGN(auto ant1,
+                       ArrayFromJSON(arrow::int32(), "[1, 1, 1, 2, 2, 2, 3, 3, 3]"));
+  ASSERT_OK_AND_ASSIGN(auto ant2,
+                       ArrayFromJSON(arrow::int32(), "[1, 1, 1, 2, 2, 2, 3, 3, 3]"));
+  ASSERT_OK_AND_ASSIGN(auto rows,
+                       ArrayFromJSON(arrow::int64(), "[1, 2, 3, 4, 5, 6, 7, 8, 9]"));
+
+  ASSERT_OK_AND_ASSIGN(auto base, GroupSortData::Make(groups, time, ant1, ant2, rows));
+  ASSERT_OK_AND_ASSIGN(auto sorted, base->Sort());
+  ASSERT_OK_AND_ASSIGN(auto merged, MergeGroups({sorted, sorted}));
+}
+
+}  // namespace
