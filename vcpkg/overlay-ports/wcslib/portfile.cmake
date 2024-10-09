bc9c65d58583f5f0a69ef925d46fd9d844175186
Simon Perkins <simon.perkins@gmail.com>
2024-10-09 20:28:07 +0200

    fixes

--- vcpkg/overlay-ports/wcslib/portfile.cmake
+++ vcpkg/overlay-ports/wcslib/portfile.cmake
@@ -2,14 +2,14 @@ vcpkg_download_distfile(archive
     URLS "http://www.atnf.csiro.au/people/mcalabre/WCS/wcslib-8.3.tar.bz2"
     FILENAME "wcslib-8.3.tar.bz2"
     SHA512 248518489431cbcba7a5df9e34a747e2a007128639d8ab655ceee35250e609d952fe466b67cdca5defe16d3e926730d00bfc5c362c369f74851cd88973b506ba
-    PATCHES
-        001-modern-configs.patch
 )
 
 
 vcpkg_extract_source_archive(
     src
     ARCHIVE "${archive}"
+    PATCHES
+        001-modern-configs.patch
 )
 
 vcpkg_configure_make(
