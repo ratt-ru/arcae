eeb0618447be589639a481d0d8c73107ed4e3ebb
Simon Perkins <simon.perkins@gmail.com>
2024-10-09 16:34:19 +0200

    Try everything from a conda environment

--- ci/scripts/run_cibuildwheel.sh
+++ ci/scripts/run_cibuildwheel.sh
@@ -6,22 +6,26 @@ set -x
 # OpenMP is not present on macOS by default
 if [[ $(uname) == "Darwin" ]]; then
   export MACOSX_DEPLOYMENT_TARGET=12.0
-  if [[ "$CIBW_BUILD" == *-macosx_arm64 ]]; then
-    OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-arm64/llvm-openmp-11.1.0-hf3c4609_1.tar.bz2"
-  else
-    OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-64/llvm-openmp-11.1.0-hda6cdc1_1.tar.bz2"
-  fi
+  # if [[ "$CIBW_BUILD" == *-macosx_arm64 ]]; then
+  #   OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-arm64/llvm-openmp-11.1.0-hf3c4609_1.tar.bz2"
+  # else
+  #   OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-64/llvm-openmp-11.1.0-hda6cdc1_1.tar.bz2"
+  # fi
 
   # This depends on miniforge installation
-  conda create -n arcae-build $OPENMP_URL
-  PREFIX="$CONDA_HOME/envs/arcae-build"
+  # conda create -n arcae-build $OPENMP_URL
+  # PREFIX="$CONDA_HOME/envs/arcae-build"
 
-  export CC=/usr/bin/clang
-  export CXX=/usr/bin/clang++
-  export CPPFLAGS="$CPPFLAGS -Xpreprocessor -fopenmp"
-  export CFLAGS="$CFLAGS -I$PREFIX/include"
-  export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
-  export LDFLAGS="$LDFLAGS -Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -lomp"
+  # export CC=/usr/bin/clang
+  # export CXX=/usr/bin/clang++
+  # export CPPFLAGS="$CPPFLAGS -Xpreprocessor -fopenmp"
+  # export CFLAGS="$CFLAGS -I$PREFIX/include"
+  # export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
+  # export LDFLAGS="$LDFLAGS -Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -lomp"
+
+  conda create -n arcae-build llvm-openmp compilers python
+  conda init
+  conda activate arcae-build
 fi
 
 python -m pip install cibuildwheel
