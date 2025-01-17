c5716fe9596bb80f5719989500e69dc0b1f11ebe
Simon Perkins <simon.perkins@gmail.com>
2025-01-17 18:14:51 +0200

    Remove hard-coded LIBRARY_PATH

--- ci/scripts/run_cibuildwheel.sh
+++ ci/scripts/run_cibuildwheel.sh
@@ -14,7 +14,6 @@ if [[ $(uname) == "Darwin" ]]; then
   source "${MINIFORGE_PATH}/etc/profile.d/conda.sh"
   conda create -n arcae-build -c conda-forge compilers llvm-openmp python
   conda activate arcae-build
-  export LIBRARY_PATH=/usr/lib:$LIBRARY_PATH
 fi
 
 python -m pip install cibuildwheel
