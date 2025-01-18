07865df47c61bd271463649f0b4eb87c5251fb56
Simon Perkins <simon.perkins@gmail.com>
2025-01-18 08:55:20 +0200

    Update build toolchain to vcpkg 2024.12.15 (#138)

---
+++ ci/scripts/run_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/ash
+
+OS=$(uname -s)
+ARCH=$(uname -m)
+
+if [[ "$OS" == "Linux" && "$ARCH" == "x86_64" ]]; then
+  # We can only rely on the python-casacore wheels for linux x86
+  echo "Running complete test suite on $OS $ARCH"
+  python -m pytest -s -vvv --pyargs arcae
+else
+  echo "Running sanity check test on $OS $ARCH"
+  python -c "from arcae.testing import sanity; sanity()"
+fi
