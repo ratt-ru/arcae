0b3ed7a6eadc09fad2cf47099ad7fb39643172d6
Simon Perkins <simon.perkins@gmail.com>
2026-06-12 13:21:37 +0200

    Remove python-casacore guards in arcae.__init__

--- src/arcae/__init__.py
+++ src/arcae/__init__.py
@@ -1,27 +1,11 @@
 # Needed to load in the arrow c++ shared libraries
 import pyarrow as pa  # noqa
-import os
-import sys
 from typing import Union, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from arcae.lib.arrow_tables import Table
 
 __version__ = "0.5.1"
-
-PYTHON_CASACORE_FOUND = "casacore" in sys.modules
-COEXIST_WITH_PYTHON_CASACORE = int(os.environ.get("ARCAE_WITH_CASACORE", 0)) != 0
-
-if PYTHON_CASACORE_FOUND and not COEXIST_WITH_PYTHON_CASACORE:
-    raise RuntimeError(
-        "python-casacore has already been imported and "
-        "this may lead to extension module symbol conflicts "
-        "and segfaults."
-        "See https://github.com/ratt-ru/arcae/issues/72 "
-        "for further information. "
-        "Set ARCAE_WITH_CASACORE=1 if you wish to "
-        "continue regardless."
-    )
 
 
 def safe_multithreaded_writes() -> bool:
